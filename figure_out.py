463c2ffd6d533cdfca003442cebf41ae4327d1a9
RockinWool <tamkaz23@yahoo.co.jp>
2020-04-27 18:17:57 +0900

    png出力の調整

--- figure_out.py
+++ figure_out.py
@@ -5,5 +5,6 @@ def figure_out(G,pos,name):
     nx.draw_networkx_nodes(G, pos, node_size=20, node_color="b")
     nx.draw_networkx_edges(G, pos, width=1)
     plt.axis("off")
-    plt.show()
     plt.savefig( '{}.png'.format(name))
+    plt.show()
+    
